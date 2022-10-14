237215e6318beca8726009d1a86a02404de04035
Akshay <akshay@Akshays-MacBook-Pro.local>
2022-10-14 15:36:10 +0530

    first script

---
+++ Test.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Mysql123@"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE test1")
